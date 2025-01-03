391591955a1651e85421a53360e82ba9cc5cea92
lshAlgorithm <lishuhuai_brian@163.com>
2025-01-03 22:12:26 +0800

    finish 4_4

--- exp_4_4_custom_pytorch_op_student/stu_upload/op_hsigmoid/hsigmoid.cpp
+++ exp_4_4_custom_pytorch_op_student/stu_upload/op_hsigmoid/hsigmoid.cpp
@@ -5,21 +5,27 @@ using namespace std;
 //hsigmoid_cpu函数的具体实现
 torch::Tensor hsigmoid_cpu(const torch::Tensor & dets) {
   //TODO: 将输入的tensor转化为浮点类型的vector
-  ______________________________________
-  int input_size = input_data.size(); 
+  auto input = dets.accessor<float, 2>();
+  int batch_size = input.size(0);
+  int input_size = input.size(1); 
   //TODO: 创建一个浮点类型的output_data，output_data为大小与输入相同的vector
-  ______________________________________
+  vector<float> output_data(batch_size * input_size);
   //TODO: 对于输入向量的每个元素计算hsigmoid
-  ______________________________________
+  for (int i = 0; i < batch_size; i++) {
+    for (int j = 0; j < input_size; j++) {
+      output_data[i * input_size + j] = 
+        std::min(1.0, std::max(.0, input[i][j] / 6.0f + 0.5f));
+    }
+  }
   //TODO: Create tensor options with dtype float32
   auto opts = torch::TensorOptions().dtype(torch::kFloat32);
   //TODO: Create a tensor from the output vector
   auto foo= torch::from_blob(output_data.data(), {int64_t(output_data.size())}, opts).clone();
   //TODO: 将得到的tensor reshape为所需的大小
-  ______________________________________
+  output = foo.reshape([batch_size, input_size]);
   return output;
 } 
 //TODO: 算子绑定为Pytorch的模块
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {	
-______________________________________
+m.def("hsigmoid_cpu", &hsigmoid_cpu, "HSigmoid activation function (CPU)");
 }       
